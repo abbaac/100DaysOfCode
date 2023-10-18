4e18bcf535de589e3606ac695be84474e03e5c7d
Abba00 <aaliconcern@gmail.com>
2023-10-19 00:50:01 +0100

    day6

---
+++ day6-maze/Maze.py
@@ -0,0 +1,26 @@
+# Refer to https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# Refert to link above for the question of this solution solves
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
