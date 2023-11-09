e429e9f3648e9cd4358e5a1304924ba363f47a81
Abba00 <aaliconcern@gmail.com>
2023-11-09 14:47:47 +0100

    day 13

---
+++ day13-debugging/debug.py
@@ -0,0 +1,20 @@
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#     b_list.append(new_item)
+#     print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+#Fix bugged code above
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+mutate([1,2,3,5,8,13])
+
